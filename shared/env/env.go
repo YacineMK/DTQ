281b5632978b329ab02d6c5a753986ab0a3603e9
YacineMK <merroukiyacine06@gmail.com>
2025-09-26 17:22:08 +0100

    wip[shared]: add env package in shared folder

---
+++ shared/env/env.go
@@ -0,0 +1,15 @@
+
+package env
+
+import (
+	"os"
+)
+
+
+func GetEnv(key string , defultValue string) string {
+	value , ok := os.LookupEnv(key)
+	if !ok {
+		return defultValue
+	}
+	return value
+}
